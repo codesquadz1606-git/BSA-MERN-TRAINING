d458e2d0b087af4bb18572d5b357e07f64a11d1f
codesquadz1606-git <codesquadz1606@gmail.com>
2026-06-29 12:54:08 +0530

    React Lecture 1

---
+++ Frontend Engg/React/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import {ConditionRendering, StateComponent, ThemeComponent} from './Class 1/First.jsx';
+import First, { First1 } from "./Class 1/First.jsx";
+import PropsComponent from './Class 1/Props.jsx';
+function App() {
+  return (
+    <>
+        {/* <First/>
+        <First1/> */}
+        {/* <StateComponent/> */}
+        {/* <ConditionRendering/> */}
+        {/* <ThemeComponent/> */}
+        <PropsComponent/>
+    </>
+  )
+}
+
+export default App
